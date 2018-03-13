1ecdf48caca25b30212e0c3ed9f93050b4fc0d13
Hiroshi Siqueira <hiroshi.siq@gmail.com>
2018-03-13 19:25:01 -0300

    Add camera to flask server

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, Response
+from camera import VideoCamera
 
 app = Flask(__name__)
 
@@ -9,6 +10,17 @@ def index():
 @app.route('/webcam')
 def webcam_page():
 	return render_template('webcam.html')
+
+def genarateImage(camera):
+	while True:
+		frame = camera.get_frame()
+		yield(b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n'))
+
+@app.route('/test')
+def test_page():
+	return Response(genarateImage(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
 	app.run(debug=True, host='10.42.14.232', port=int("8080"))
