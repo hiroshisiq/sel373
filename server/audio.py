077eaab3e36604b1a50d78d942281bcee04ecfec
Hiroshi Siqueira <hiroshi.siq@gmail.com>
2018-04-15 23:42:05 +0000

    Audio streaming script

--- server/audio.py
+++ server/audio.py
@@ -12,9 +12,9 @@ import wave
 #record
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
-CHANNELS = 1
+CHANNELS = 2
 WAVE_OUTPUT_FILENAME = "teste.wav"
-RATE = 48000
+RATE = 44100
 RECORD_SECONDS = 5
 
 p = pyaudio.PyAudio()
@@ -23,31 +23,38 @@ stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
-                output=True,
                 frames_per_buffer=CHUNK)
 
 
 frames = []
 
 for i in range(0, int(RATE/CHUNK*RECORD_SECONDS)):
-	data  = stream.read(CHUNK, exception_on_overflow=False)
-	frames.append(data)
-   	stream.write(data)
-#	yield data 
+    data  = stream.read(CHUNK)
+#    frames.append(data)
+    frames = data
+    
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    
+#    yield data 
 
 
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+#wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+#wf.setnchannels(CHANNELS)
+#wf.setsampwidth(p.get_sample_size(FORMAT))
+#wf.setframerate(RATE)
+#wf.writeframes(b''.join(frames))
+#wf.close()
 
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+#stream.stop_stream()
+#stream.close()
+#p.terminate()
 
 
 
